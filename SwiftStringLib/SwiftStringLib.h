cadaae23357e83549dac7f8f83a64ae907173b60
jay_bellway <jayant.bellway@gmail.com>
2019-06-20 13:54:44 +0530

    Initial project setup.

---
+++ SwiftStringLib/SwiftStringLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftStringLib.h
+//  SwiftStringLib
+//
+//  Created by jayant patidar on 20/06/19.
+//  Copyright © 2019 IntellArc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftStringLib.
+FOUNDATION_EXPORT double SwiftStringLibVersionNumber;
+
+//! Project version string for SwiftStringLib.
+FOUNDATION_EXPORT const unsigned char SwiftStringLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStringLib/PublicHeader.h>
+
+
